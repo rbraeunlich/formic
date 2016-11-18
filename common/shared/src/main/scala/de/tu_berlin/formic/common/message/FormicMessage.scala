30777ca8bb2ac1b6d3f360a715f5373616a2d09a
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-11-18 10:57:58 +0100

    add comments to FormicMessages

--- common/shared/src/main/scala/de/tu_berlin/formic/common/message/FormicMessage.scala
+++ common/shared/src/main/scala/de/tu_berlin/formic/common/message/FormicMessage.scala
@@ -11,14 +11,25 @@ import upickle.Js
 sealed trait FormicMessage
 
 /**
-  * A response from the server, indicating that a data type instance has been created.
+  * A response from the server, acknowledging to the client that the data type has been instantiated.
   *
-  * @author Ronny Bräunlich
   */
 case class CreateResponse(dataTypeInstanceId: DataTypeInstanceId) extends FormicMessage
 
+/**
+  * A request from the client to create a new data type instance
+  * @param clientId the client that created the data type instance
+  * @param dataTypeInstanceId the id the client gave the data type
+  * @param dataType the name of the data type, needed to find the proper factory
+  */
 case class CreateRequest(clientId: ClientId, dataTypeInstanceId: DataTypeInstanceId, dataType: DataTypeName) extends FormicMessage
 
+/**
+  * If a client was disconnected and missed some operations it can send this request.
+  * @param clientId The client that is missing operations
+  * @param dataTypeInstanceId The data type the client needs the operations for
+  * @param sinceId the operation id of the last operation the client knows about
+  */
 case class HistoricOperationRequest(clientId: ClientId, dataTypeInstanceId: DataTypeInstanceId, sinceId: OperationId) extends FormicMessage
 
 /**
@@ -36,4 +47,13 @@ case class UpdateResponse(dataTypeInstanceId: DataTypeInstanceId, dataType: Data
   */
 case class UpdateRequest(clientId: ClientId, dataTypeInstanceId: DataTypeInstanceId) extends FormicMessage
 
+/**
+  * A message containing operations. If a client made a normal change to a data type instance this message contains
+  * a single operations. After being applied the same message is distributed to all clients. This message can
+  * also be an answer to a HistoricOperationRequest. In that case it can contain zero to n operations.
+  * @param clientId The client that applied the operation or that want to receive operations
+  * @param dataTypeInstanceId the data type that changed
+  * @param dataType the type of the data type that changed, needed for JSON deserialization
+  * @param operations the operation/s that shall be applied
+  */
 case class OperationMessage(clientId: ClientId, dataTypeInstanceId: DataTypeInstanceId, dataType: DataTypeName, operations: List[DataTypeOperation]) extends FormicMessage
