5d40bf8e9f1a2837ca081fd8f33d0ba55e21ad88
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-11-30 10:15:47 +0100

    add comment to FormicJsonDataTypeProtocol

--- common/shared/src/main/scala/de/tu_berlin/formic/common/json/FormicJsonDataTypeProtocol.scala
+++ common/shared/src/main/scala/de/tu_berlin/formic/common/json/FormicJsonDataTypeProtocol.scala
@@ -3,13 +3,17 @@ package de.tu_berlin.formic.common.json
 import de.tu_berlin.formic.common.datatype.{DataTypeName, DataTypeOperation}
 
 /**
+  * Only the concrete data types can know about the structure of their operations. Therefore,
+  * they are responsible to provide a FormicJsonDataTypeProtocol that implements those methods.
+
   * @author Ronny Bräunlich
   */
 trait FormicJsonDataTypeProtocol {
 
   def deserializeOperation(json: String): DataTypeOperation
 
+  def serializeOperation(op: DataTypeOperation): String
+
   val name: DataTypeName
 
-  def serializeOperation(op: DataTypeOperation): String
 }
