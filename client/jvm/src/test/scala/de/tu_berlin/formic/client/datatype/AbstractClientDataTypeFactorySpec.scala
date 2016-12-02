1b320ca8c914b046b0af3d6a85a956b0dfb1caf0
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-12-02 14:03:03 +0100

    create test for AbstractClientDataTypeFactory

---
+++ client/jvm/src/test/scala/de/tu_berlin/formic/client/datatype/AbstractClientDataTypeFactorySpec.scala
@@ -0,0 +1,67 @@
+package de.tu_berlin.formic.client.datatype
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit, TestProbe}
+import de.tu_berlin.formic.client.DataTypeInstantiator.WrappedCreateRequest
+import de.tu_berlin.formic.client.datatype.AbstractClientDataTypeFactory.NewDataTypeCreated
+import de.tu_berlin.formic.client.{StopSystemAfterAll, TestControlAlgorithm}
+import de.tu_berlin.formic.common.{ClientId, DataTypeInstanceId}
+import de.tu_berlin.formic.common.datatype._
+import de.tu_berlin.formic.common.message.CreateRequest
+import org.scalatest.{Matchers, WordSpecLike}
+
+/**
+  * @author Ronny Bräunlich
+  */
+class AbstractClientDataTypeFactorySpec extends TestKit(ActorSystem("AbstractClientDataTypeFactorySpec"))
+  with WordSpecLike
+  with ImplicitSender
+  with StopSystemAfterAll
+  with Matchers {
+
+  "AbstractClientDataTypeFactory" must {
+
+    "create a data type and data type wrapper when receiving a WrappedCreateRequest" in {
+      val factory = system.actorOf(Props(new AbstractClientDataTypeFactorySpecFactory))
+      val outgoing = TestProbe()
+      val instanceId = DataTypeInstanceId()
+
+      factory ! WrappedCreateRequest(outgoing.ref, CreateRequest(ClientId(), instanceId, DataTypeName("AbstractClientDataTypeFactorySpec") ))
+
+      val msg = expectMsgClass(classOf[NewDataTypeCreated])
+      msg.dataTypeInstanceId should equal(instanceId)
+      msg.dataTypeActor should not equal null
+      msg.wrapper shouldBe a[AbstractClientDataTypeFactorySpecFormicDataType]
+    }
+
+  }
+
+}
+
+class AbstractClientDataTypeFactorySpecDataType extends AbstractDataType(DataTypeInstanceId(), TestControlAlgorithm) {
+  override val dataTypeName: DataTypeName = DataTypeName("AbstractClientDataTypeFactorySpec")
+
+  override val transformer: OperationTransformer = null
+
+  override def apply(op: DataTypeOperation): Unit = {}
+
+  override def getDataAsJson: String = ""
+}
+
+class AbstractClientDataTypeFactorySpecFormicDataType extends FormicDataType {
+  override val dataTypeName: DataTypeName = DataTypeName("AbstractClientDataTypeFactorySpec")
+  override var callback: () => Unit = _
+}
+
+class AbstractClientDataTypeFactorySpecFactory extends AbstractClientDataTypeFactory[AbstractClientDataTypeFactorySpecDataType, AbstractClientDataTypeFactorySpecFormicDataType](null) {
+
+  override val name: DataTypeName = DataTypeName("AbstractClientDataTypeFactorySpec")
+
+  override def createDataType(dataTypeInstanceId: DataTypeInstanceId, outgoingConnection: ActorRef): AbstractClientDataTypeFactorySpecDataType = {
+    new AbstractClientDataTypeFactorySpecDataType
+  }
+
+  override def createWrapperType(dataTypeInstanceId: DataTypeInstanceId, dataType: ActorRef): AbstractClientDataTypeFactorySpecFormicDataType = {
+    new AbstractClientDataTypeFactorySpecFormicDataType
+  }
+}
