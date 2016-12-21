69a0b0f21217a1a7e176d551fe52697a47e447a8
Ronny Bräunlich <r.braeunlich@gmail.com>
2016-12-21 21:19:13 +0100

    made states more readable

--- websockettests/js/src/test/scala/de/tu_berlin/formic/client/WebSocketFactorySpec.scala
+++ websockettests/js/src/test/scala/de/tu_berlin/formic/client/WebSocketFactorySpec.scala
@@ -1,5 +1,7 @@
 package de.tu_berlin.formic.client
 
+import de.tu_berlin.formic.client.WebSocketFactorySpec.{WebSocketClosedState, WebSocketClosingState, WebSocketConnectingState, WebSocketOpenState}
+
 import scala.scalajs.js.timers._
 import org.scalajs.dom._
 import org.scalajs.dom.raw.WebSocket
@@ -20,11 +22,41 @@ class WebSocketFactorySpec extends FlatSpec with Matchers {
     }
     connection.onmessage = { event: MessageEvent => println(s"WebSocketFactorySpec: message received: $event") }
 
-    setTimeout(1000) {
-      connection.readyState should equal(WebSocket.OPEN)
+    setTimeout(500) {
+      getConnectionObject(connection.readyState) should equal(WebSocketFactorySpec.WebSocketOpenState)
     }
     setTimeout(2000) {
       connection.close()
     }
+  }
+
+  /**
+    * This method and the states only serve the readability of the TestFailedException
+    */
+  def getConnectionObject(readyState: Int) = {
+    readyState match {
+      case 0 => WebSocketConnectingState
+      case 1 => WebSocketOpenState
+      case 2 => WebSocketClosingState
+      case 3 => WebSocketClosedState
+    }
+  }
+}
+
+object WebSocketFactorySpec {
+  sealed trait WebSocketState {
+    val stateNr: Int
+  }
+  case object WebSocketConnectingState extends WebSocketState {
+    override val stateNr: Int = 0
+  }
+  case object WebSocketOpenState extends WebSocketState {
+    override val stateNr: Int = 1
+  }
+  case object WebSocketClosingState extends WebSocketState {
+    override val stateNr: Int = 2
+  }
+  case object WebSocketClosedState extends WebSocketState {
+    override val stateNr: Int = 3
   }
 }
