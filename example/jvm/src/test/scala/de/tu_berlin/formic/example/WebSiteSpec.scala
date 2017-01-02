6b1595313c689bc3237483253e0c8282ec8bb3f4
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-02 20:17:23 +0100

    un-ignore all Selenium tests

--- example/jvm/src/test/scala/de/tu_berlin/formic/example/WebSiteSpec.scala
+++ example/jvm/src/test/scala/de/tu_berlin/formic/example/WebSiteSpec.scala
@@ -81,7 +81,7 @@ class WebSiteSpec extends FlatSpec
     click on id("new-tree-button")
   }
 
-  "The button to create a tree" should "create a div containing input, buttons and a list" ignore {
+  "The button to create a tree" should "create a div containing input, buttons and a list" in {
     go to host + "/index"
     click on id("new-tree-button")
     Thread.sleep(2000)
@@ -98,7 +98,7 @@ class WebSiteSpec extends FlatSpec
     xpath(s"//div[@id='$treeId']/div/ul/li").findElement.get.text should be("empty")
   }
 
-  "A single user" should "be able to modify the tree" ignore {
+  "A single user" should "be able to modify the tree" in {
     go to host + "/index"
     click on id("new-tree-button")
     Thread.sleep(5000)
@@ -111,7 +111,7 @@ class WebSiteSpec extends FlatSpec
     xpath(s"//div[@id='$treeId']/div/ul/li").findElement.get.text should be("2")
   }
 
-  "A second user" should "be able to subscribe to a tree" ignore {
+  "A second user" should "be able to subscribe to a tree" in {
     go to host + "/index"
     click on id("new-tree-button")
     Thread.sleep(5000)
@@ -129,7 +129,7 @@ class WebSiteSpec extends FlatSpec
     secondUserDriver.quit()
   }
 
-  "Two users" should "be able to concurrently edit the tree" ignore {
+  "Two users" should "be able to concurrently edit the tree" in {
     go to host + "/index"
     click on id("new-tree-button")
     Thread.sleep(5000)
