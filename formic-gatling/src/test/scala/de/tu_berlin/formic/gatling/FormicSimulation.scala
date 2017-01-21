f12bb5e9abb70d95ad7107506e2909d3085ae463
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-21 16:30:44 +0100

    applying more parallel users

--- formic-gatling/src/test/scala/de/tu_berlin/formic/gatling/FormicSimulation.scala
+++ formic-gatling/src/test/scala/de/tu_berlin/formic/gatling/FormicSimulation.scala
@@ -16,7 +16,7 @@ class FormicSimulation extends Simulation {
     .logLevel("info")
 
   //to have a feeder for all scenarios, we create the ids up front and use them
-  val dataTypeInstanceIdFeeder = for (x <- 0.until(1)) yield Map("dataTypeInstanceId" -> DataTypeInstanceId().id)
+  val dataTypeInstanceIdFeeder = for (x <- 0.until(2)) yield Map("dataTypeInstanceId" -> DataTypeInstanceId().id)
 
   val connect = exec(formic("Connection").connect())
     .pause(2)
@@ -27,7 +27,8 @@ class FormicSimulation extends Simulation {
     .linear("${dataTypeInstanceId}"))
     .pause(5)
 
-  val edit = repeat(10, "n") {
+  val edit = //feed(dataTypeInstanceIdFeeder.iterator.toArray.random)
+    repeat(10, "n") {
     exec(formic("LinearInsertion")
       .linear("${dataTypeInstanceId}")
       .insert('a')
@@ -51,8 +52,8 @@ class FormicSimulation extends Simulation {
     .exec(connect, subscribe, edit)
 
   setUp(
-    creators.inject(atOnceUsers(1)),
-    editors.inject(atOnceUsers(1))
+    creators.inject(atOnceUsers(2)),
+    editors.inject(atOnceUsers(2))
   ).protocols(formicConfig)
 
 }
