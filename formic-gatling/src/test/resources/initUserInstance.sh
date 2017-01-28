4a87320f7c7b933df275cedd796a4796d6caedbb
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-28 10:53:05 +0100

    fix script
    -line containing apt instead of apt-get fixed
    -set frontend variable before every install command to fix "unable to initialize frontend" error

--- formic-gatling/src/test/resources/initUserInstance.sh
+++ formic-gatling/src/test/resources/initUserInstance.sh
@@ -1,10 +1,15 @@
-#install java
+#!/usr/bin/env bash
 sudo apt-get update
-sudo apt --yes install openjdk-8-jdk openjdk-8-demo openjdk-8-doc openjdk-8-jre-headless openjdk-8-source
+#install necessary packages
+echo "Install Java"
+sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install openjdk-8-jdk openjdk-8-demo openjdk-8-doc openjdk-8-jre-headless openjdk-8-source
+echo "Install Unzip"
+sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install unzip
 
 #Download Gatling
+echo "Download Gatling"
 curl -k https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.2.3/gatling-charts-highcharts-bundle-2.2.3-bundle.zip -o gatling.zip
-unzip gatling.zip -d gatling
-cd gatling/gatling-charts-highcharts-bundle-2.2.3
+unzip gatling.zip
+cd gatling-charts-highcharts-bundle-2.2.3
 
 #download formic assembly jars
