3ab921efdb097951b120b895daad0b061b87acbd
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-28 10:59:10 +0100

    add cd so that Gatling is not downloaded into /

--- formic-gatling/src/test/resources/initUserInstance.sh
+++ formic-gatling/src/test/resources/initUserInstance.sh
@@ -8,6 +8,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get --yes install unzip
 
 #Download Gatling
 echo "Download Gatling"
+cd ~
 curl -k https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.2.3/gatling-charts-highcharts-bundle-2.2.3-bundle.zip -o gatling.zip
 unzip gatling.zip
 cd gatling-charts-highcharts-bundle-2.2.3
