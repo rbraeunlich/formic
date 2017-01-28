fe26b04a8e7cbc2c4a1dad830407a2882efb9edb
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-28 12:02:01 +0100

    change permissions of gatling files

--- formic-gatling/src/test/resources/initUserInstance.sh
+++ formic-gatling/src/test/resources/initUserInstance.sh
@@ -11,6 +11,4 @@ echo "Download Gatling"
 cd /home/ubuntu
 curl -k https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.2.3/gatling-charts-highcharts-bundle-2.2.3-bundle.zip -o gatling.zip
 unzip gatling.zip
-cd gatling-charts-highcharts-bundle-2.2.3
-
-#download formic assembly jars
+sudo chmod -R 777 gatling-charts-highcharts-bundle-2.2.3/
