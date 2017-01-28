48cc978a0e54c0777783bd3555bee5e191226f0e
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-28 11:00:06 +0100

    add env variables, ssh with -i option

--- formic-gatling/src/test/resources/executeGatlingTest.sh
+++ formic-gatling/src/test/resources/executeGatlingTest.sh
@@ -14,17 +14,24 @@ USER_NAME='ubuntu'
 #Remote hosts list
 HOSTS=( 192.168.28.24 192.123.123.12 180.123.98.1)
 
+#Simulation options
+NUM_EDITORS=1
+DATA_TYPE_ID=''
+JAVA_OPTS="-DformicEditors=$NUM_EDITORS -DformicId=$DATA_TYPE_ID"
+export JAVA_OPTS
+
 #Assuming all Gatling installation in same path (with write permissions)
-GATLING_HOME=/gatling/gatling-charts-highcharts-1.5.6
+GATLING_HOME=/gatling-charts-highcharts-bundle-2.2.3
 GATLING_SIMULATIONS_DIR=$GATLING_HOME/user-files/simulations
 GATLING_RUNNER=$GATLING_HOME/bin/gatling.sh
 
 #Change to your simulation class name
-SIMULATION_NAME='nimrodstech.GatlingClusterTest'
+SIMULATION_NAME='de.tu_berlin.formic.gatling.experiment.LinearInsertSimulation'
 
 #No need to change this
 GATLING_REPORT_DIR=$GATLING_HOME/results/
 GATHER_REPORTS_DIR=/gatling/reports/
+GATLING_LIB_DIR=$GATLING_HOME/lib
 
 echo "Starting Gatling cluster run for simulation: $SIMULATION_NAME"
 
@@ -36,20 +43,21 @@ rm -rf $GATLING_REPORT_DIR
 for HOST in "${HOSTS[@]}"
 do
   echo "Cleaning previous runs from host: $HOST"
-  ssh -n -f $USER_NAME@$HOST "sh -c 'rm -rf $GATLING_REPORT_DIR'"
+  ssh -n -f -i cloud.key $USER_NAME@$HOST "sh -c 'rm -rf $GATLING_REPORT_DIR'"
 done
 
 for HOST in "${HOSTS[@]}"
 do
-  echo "Copying simulations to host: $HOST"
-  scp -r $GATLING_SIMULATIONS_DIR/* $USER_NAME@$HOST:$GATLING_SIMULATIONS_DIR
+  echo "Copying simulation JARs to host: $HOST"
+  scp -i cloud.key $GATLING_LIB_DIR/formic-gatling-assembly-1.0.0 $USER_NAME@$HOST:$GATLING_SIMULATIONS_DIR
+  scp -i cloud.key $GATLING_LIB_DIR/formic-gatling-test-1.0.0 $USER_NAME@$HOST:$GATLING_SIMULATIONS_DIR
 done
 
 for HOST in "${HOSTS[@]}"
 do
   echo "Running simulation on host: $HOST"
   export TMPDIR=/tmp
-  ssh -n -f $USER_NAME@$HOST "sh -c 'nohup $GATLING_RUNNER -nr -s $SIMULATION_NAME > /gatling/run.log 2>&1 &'"
+  ssh -n -f -i cloud.key $USER_NAME@$HOST "sh -c 'nohup $GATLING_RUNNER -nr -s $SIMULATION_NAME > /gatling/run.log 2>&1 &'"
 done
 
 echo "Running simulation on localhost"
@@ -63,7 +71,7 @@ cp ${GATLING_REPORT_DIR}report/simulation.log $GATHER_REPORTS_DIR
 for HOST in "${HOSTS[@]}"
 do
   echo "Gathering result file from host: $HOST"
-  ssh -n -f $USER_NAME@$HOST "sh -c 'ls -t $GATLING_REPORT_DIR | head -n 1 | xargs -I {} mv ${GATLING_REPORT_DIR}{} ${GATLING_REPORT_DIR}report'"
+  ssh -n -f -i cloud.key $USER_NAME@$HOST "sh -c 'ls -t $GATLING_REPORT_DIR | head -n 1 | xargs -I {} mv ${GATLING_REPORT_DIR}{} ${GATLING_REPORT_DIR}report'"
   scp $USER_NAME@$HOST:${GATLING_REPORT_DIR}report/simulation.log ${GATHER_REPORTS_DIR}simulation-$HOST.log
 done
 
