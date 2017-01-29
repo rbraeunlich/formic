ed63ac85d3d2cc4206a37071a03baf2f61efa863
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-29 10:17:04 +0100

    add preparation script

--- formic-gatling/src/test/resources/moveFilesToCoordinator.sh
+++ formic-gatling/src/test/resources/moveFilesToCoordinator.sh
@@ -8,8 +8,10 @@ ASSEMBLY=/Users/ronny/Entwicklung/Masterthesis/scala-workspace/formic/formic-gat
 TEST_ASSEMBLY=/Users/ronny/Entwicklung/Masterthesis/scala-workspace/formic/formic-gatling/target/scala-2.11/formic-gatling-test-1.0.0.jar
 KEY=/Users/ronny/cloud.key
 EXECUTION_SCRIPT=/Users/ronny/Entwicklung/Masterthesis/scala-workspace/formic/formic-gatling/src/test/resources/executeGatlingTest.sh
+PREPARATION_SCRIPT=/Users/ronny/Entwicklung/Masterthesis/scala-workspace/formic/formic-gatling/src/test/resources/executeGatlingTestWithPreparation.sh
 
 scp -i $KEY $ASSEMBLY $USER_NAME@$COORDINATOR:$GATLING_LIB_DIR
 scp -i $KEY $TEST_ASSEMBLY $USER_NAME@$COORDINATOR:$GATLING_LIB_DIR
 scp -i $KEY $KEY $USER_NAME@$COORDINATOR:$USER_HOME
 scp -i $KEY $EXECUTION_SCRIPT $USER_NAME@$COORDINATOR:$USER_HOME
+scp -i $KEY $PREPARATION_SCRIPT $USER_NAME@$COORDINATOR:$USER_HOME
