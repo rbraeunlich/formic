a556c76242f613a2f80bad874738462b953bb477
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-29 10:16:23 +0100

    write preparation output into log

--- formic-gatling/src/test/resources/executeGatlingTestWithPreparation.sh
+++ formic-gatling/src/test/resources/executeGatlingTestWithPreparation.sh
@@ -3,6 +3,7 @@
 FORMIC_SERVER="http://10.200.1.67:80"
 JAVA_OPTS="-DformicServer=$FORMIC_SERVER"
 export JAVA_OPTS
-DATATYPEID=`./gatling-charts-highcharts-bundle-2.2.1/bin/gatling.sh -s de.tu_berlin.formic.gatling.experiment.LinearInsertPreparationSimulation | grep "Id" | tr -d "Id: "`
+gatling-charts-highcharts-bundle-2.2.1/bin/gatling.sh -s de.tu_berlin.formic.gatling.experiment.LinearInsertPreparationSimulation > preparation.log
+DATATYPEID=`cat preparation.log | grep "Id" | tr -d "Id: "`
 echo "Id: $DATATYPEID"
-./executeGatlingTest.sh $1 $DATATYPEID
+./executeGatlingTest.sh $1 ${DATATYPEID}
