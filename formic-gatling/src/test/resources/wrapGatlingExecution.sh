d70b6d027dde6669b6dbeb538967dfd30fd90953
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-01-29 13:48:52 +0100

    forgot to increment the other numbers

--- formic-gatling/src/test/resources/wrapGatlingExecution.sh
+++ formic-gatling/src/test/resources/wrapGatlingExecution.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 GATLING_HOME=gatling-charts-highcharts-bundle-2.2.1
 JAVA_OPTS="$1 $2 $3 $4"
-GATLING_RUNNER=$4
-SIMULATION_NAME=$5
+GATLING_RUNNER=$5
+SIMULATION_NAME=$6
 
 . ./$GATLING_RUNNER -nr -s $SIMULATION_NAME > gatling-run.log 2>&1 &
