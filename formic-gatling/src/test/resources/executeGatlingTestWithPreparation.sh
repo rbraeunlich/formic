4e646b6b23f6f4e2cac2f5318f8474def7e4f6aa
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-02-11 16:11:26 +0100

    remove *WithPreparation script because it didn't work

--- formic-gatling/src/test/resources/executeGatlingTestWithPreparation.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-FORMIC_SERVER="http://10.200.1.67:80"
-JAVA_OPTS="-DformicServer=$FORMIC_SERVER"
-export JAVA_OPTS
-gatling-charts-highcharts-bundle-2.2.1/bin/gatling.sh -s de.tu_berlin.formic.gatling.experiment.LinearInsertPreparationSimulation > preparation.log
-DATATYPEID=`cat preparation.log | grep "Id" | tr -d "Id: "`
-echo "Id: $DATATYPEID"
-./executeGatlingTest.sh $1 ${DATATYPEID}
