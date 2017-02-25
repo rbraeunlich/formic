8675ae06dd6e58f92ffc25d28031c4a35aa28187
Ronny Bräunlich <r.braeunlich@gmail.com>
2017-02-25 17:04:53 +0100

    add switch for JSON object manipulation and battleship so that both work now

--- example/js/src/main/scala/de/tu_berlin/formic/example/ExampleCallback.scala
+++ example/js/src/main/scala/de/tu_berlin/formic/example/ExampleCallback.scala
@@ -6,6 +6,7 @@ import de.tu_berlin.formic.common.datatype.{DataTypeName, FormicDataType}
 import de.tu_berlin.formic.datatype.json.client.FormicJsonObject
 import de.tu_berlin.formic.datatype.linear.client.FormicString
 import de.tu_berlin.formic.datatype.tree.client.FormicIntegerTree
+import org.scalajs.dom.document
 import org.scalajs.jquery.jQuery
 
 /**
@@ -17,7 +18,7 @@ class ExampleCallback(val main: Main) extends NewInstanceCallback {
     instance match {
       case str: FormicString => main.updateUIForString(instance.dataTypeInstanceId)
       case tree: FormicIntegerTree => main.updateUIForTree(instance.dataTypeInstanceId)
-      case json: FormicJsonObject => main.updateUIForJson(instance.dataTypeInstanceId)
+      case json: FormicJsonObject => if(isBattleShip) main.newCallbackForBattleship(json) else main.updateUIForJson(instance.dataTypeInstanceId)
     }
   }
 
@@ -34,19 +35,24 @@ class ExampleCallback(val main: Main) extends NewInstanceCallback {
 
       case tree: FormicIntegerTree =>
         main.trees += tree
-        if(jQuery("body").has("#"+tree.dataTypeInstanceId.id).length == 0){
+        if (jQuery("body").has("#" + tree.dataTypeInstanceId.id).length == 0) {
           main.insertBasicTreeElements(tree.dataTypeInstanceId.id)
         }
         main.updateUIForTree(tree.dataTypeInstanceId)(null)
 
       case json: FormicJsonObject =>
         main.jsons += json
-        if(jQuery("body").has("#"+json.dataTypeInstanceId.id).length == 0){
-          main.insertJsonManipulationElements(json.dataTypeInstanceId.id)
+        if (isBattleShip) {
+          //in order to force Battleship to update the UI we invoke the callback here manually
+          json.callback(null)
+        } else {
+          if (jQuery("body").has("#" + json.dataTypeInstanceId.id).length == 0) {
+            main.insertJsonManipulationElements(json.dataTypeInstanceId.id)
+          }
+          main.updateUIForJson(json.dataTypeInstanceId)(null)
         }
-        main.updateUIForJson(json.dataTypeInstanceId)(null)
-        //in order to force Battleship to update the UI we invoke the callback here manually
-        //json.callback(null)
     }
   }
+
+  def isBattleShip = document.location.href.contains("battleship")
 }
